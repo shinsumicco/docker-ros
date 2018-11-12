b140f35ba911d7f282c851791af35c0be9cb77b3
shinsumicco <shinsumicco@gmail.com>
2018-11-12 17:26:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ROS (kinetic) on docker
+
+ROS(kinetic)をdocker上で動かす．  
+rvizなどのOpenGLを使うGUIアプリケーションも多分動くはず．
+
+- Linux標準のディスプレイドライバを使っている場合: [standard](./standard)
+- NVIDIAのディスプレイドライバを使っている場合: [nvidia](./nvidia)
+
+参考資料:  
+[Installing and Configuring Your ROS Environment](http://wiki.ros.org/ROS/Tutorials/InstallingandConfiguringROSEnvironment)  
+[Using Hardware Acceleration with Docker](http://wiki.ros.org/docker/Tutorials/Hardware%20Acceleration)
---
+++ standard/run.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Usage:"
+echo "./run.bash [OPTIONS]"
+echo ""
+
+# 第1引数以降を繋げる
+OPTIONS=""
+for x in "$@"
+do
+    OPTIONS="$OPTIONS $x"
+done
+
+echo "OPTIONS:$OPTIONS"
+echo ""
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker run -it \
+    --user=$(id -u):$(id -g) \
+    --env=DISPLAY=$DISPLAY \
+    --env=QT_X11_NO_MITSHM=1 \
+    --workdir="/home/$USER" \
+    --volume="/home/$USER:/home/$USER" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --rm \
+    $OPTIONS \
+    ros:kinetic-desktop-standard \
+    /bin/bash
